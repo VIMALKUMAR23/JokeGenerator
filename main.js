17454e07592ba6addadcdeea78919b93921781d8
Vimal <vimal01547@gmail.com>
2024-07-20 22:38:33 +0530

    fadeOut

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const url =
   "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 let getJoke = () => {
+  jokeGen.classList.remove("fadeOut");
   fetch(url)
     .then((data) => data.json())
     .then((item) => {
       jokeGen.textContent = `${item.joke}`;
+      jokeGen.classList.add("fadeOut");
     });
 };
 btn.addEventListener("click", getJoke);
